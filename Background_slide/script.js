1492ba15ea342fd38f715e28d391302a00084186
Chelsea Alcinord <59550990+seasea19@users.noreply.github.com>
2022-01-27 16:35:48 -0500

    .

--- Background_slide/script.js
+++ Background_slide/script.js
@@ -1,38 +1,36 @@
-const body = document.body 
-const slides = document.querySelectorAll('.slide') 
-const leftBtn = document.getElementById('left') 
-const rightBtn = document.getElementById('right') 
+const body = document.body
+const slides = document.querySelectorAll('.slide')
+const leftBtn = document.getElementById('left')
+const rightBtn = document.getElementById('right')
 
-let activeSlide = 0 
+let activeSlide = 0
 
-rightBtn.addEventListener('click', () => { 
-    activeSlide .. 
-    if (activeSlide > slides.length - 1) {
-         activeSlide = 0
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+
+    if(activeSlide > slides.length){
+        activeSlide = 0
     }
-         
-         setBgToBody () 
-         setActiveSlide ()
+    setBgToBody()
+    setActiveSlide()
 })
-         
-         leftBtn.addEventListener('click', () => { 
-             activeSlide-- 
 
-             if (activeSlide < 0) { 
-                 activeSlide = slides.length - 1
-             } 
-                 setBgToBody()
+leftBtn.addEventListener('click', () => {
+    activeSlide--
 
-                  setActiveSlide() 
-             })
-                 setBgToBody() 
-                
-                 function setBgToBody () {
-                      body.style.backgroundImage = slides[activeSlide].style.backgroundImage
-                 }
-                 function setActiveSlide() { 
-                     slides.forEach((slide) => slide.classlist.remove('active'))
+    if(activeSlide < 0) {
+        activeSlide = slides.length - 1
+    }
+    setBgToBody()
+    setActiveSlide()
+})
+setBgToBody()
 
-                     slides[activeSlide].classList.add('active')
+function setBgToBody(){
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+function setActiveSlide(){
+    slides.forEach((slide) => slide.classList.remove('active'))
 
-                 }
+    slides[activeSlide].classList.add('active')
+}
